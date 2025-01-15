bb877e43038cd22a21ea30edef05c14bd15adaaa
Juan <juanluque118@gmail.com>
2025-01-15 15:33:32 +0100

    js comentado

--- js/funcionesMenu.js
+++ js/funcionesMenu.js
@@ -1,17 +1,14 @@
-//1 seleccionar nav, abrir y cerrar
-
-
-
+//Seleccion de elementos
 const nav = document.querySelector("nav");
 const abrir = document.querySelector(".abrir");
 const cerrar = document.querySelector(".cerrar");
 
-//2 al hacer click en abrir a nav se le pone la clase desplegado
+//Al hacer click en abrir a nav se le pone la clase desplegado
 abrir.addEventListener("click", function(){
     nav.className = "desplegado";
 });
 
-//3 al hacer click en cerrar a nav se le quita la clase desplegado
+//Al hacer click en cerrar a nav se le quita la clase desplegado
 cerrar.addEventListener("click", function(){
     nav.className = "";
 });
